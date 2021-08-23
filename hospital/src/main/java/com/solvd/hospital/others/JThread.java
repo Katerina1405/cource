5b3a3759af7c298582a0abce9c8513f8aded559d
Katerina Tebenkova <68323049+Katerina1405@users.noreply.github.com>
2021-08-23 22:01:53 +0300

    Add files via upload

---
+++ hospital/src/main/java/com/solvd/hospital/others/JThread.java
@@ -0,0 +1,20 @@
+package com.solvd.hospital.others;
+
+class JThread extends Thread {
+
+    JThread(String name){
+        super(name);
+    }
+
+    public void run(){
+
+        System.out.printf("%s started... \n", Thread.currentThread().getName());
+        try{
+            Thread.sleep(500);
+        }
+        catch(InterruptedException e){
+            System.out.println("Thread has been interrupted");
+        }
+        System.out.printf("%s fiished... \n", Thread.currentThread().getName());
+    }
+}
