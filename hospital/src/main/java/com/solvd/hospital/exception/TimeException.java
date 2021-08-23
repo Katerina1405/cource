8a001006eb4aa2a1cd7480310754f4221c32d317
Katerina Tebenkova <68323049+Katerina1405@users.noreply.github.com>
2021-08-23 22:00:52 +0300

    Delete hospital directory

--- hospital/src/main/java/com/solvd/hospital/exception/TimeException.java
+++
@@ -1,7 +0,0 @@
-package com.solvd.hospital.exception;
-
-public class TimeException extends NotFoundException
-{
-    public TimeException(double message) { super("\"" + message + "\" Time for work is over.");
-    }
-}
